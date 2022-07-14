6af77d2b033424c2c7e7a92e115eda7da6e63b93
ikyriak <44930763+ikyriak@users.noreply.github.com>
2022-07-14 21:01:31 +0300

    Issue-37: Fixes and Improvements to handling error and exception responses

---
+++ src/IdempotentAPI/Extensions/HttpResponseExtenions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace IdempotentAPI.Extensions
+{
+    internal static class HttpResponseExtenions
+    {
+        public static bool IsSuccessStatusCode(this HttpResponse httpResponse)
+        {
+            if (httpResponse is null)
+            {
+                return false;
+            }
+
+            return httpResponse.StatusCode >= 200 && httpResponse.StatusCode < 300;
+        }
+
+    }
+}
